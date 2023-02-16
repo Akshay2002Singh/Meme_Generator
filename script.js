7fcc27a6d1711a6edb7be0cde043dc37114c5666
Akshay2002Singh <06akshay2002@gmail.com>
2023-02-17 01:14:57 +0530

    improve loader

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 // load images  
 function getImage() {
     document.getElementById("coder_img").style = "display:None";
-    document.getElementById("loader_img").style = "display:block";
+    document.getElementById("loader_img").style = "display:block; margin: auto; min-width: 300px;padding: 0px 15px;";
     var api = `https://api.imgflip.com/get_memes`
     // origin api 
     // https://api.imgflip.com/get_memes
